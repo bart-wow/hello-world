631c406a848314406756281903fcceb1b072d9e4
bart-wow <paolo.bonetalli@gmil.com>
2017-09-23 21:33:51 +0200

    this is a test --> project committed by eclipse neon2

---
+++ asd/src/asd.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : asd.cpp
+// Author      : bp
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!bart-wow!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
